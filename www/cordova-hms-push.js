9db819c44b41a1e840725328acdbd3f10aa3e724
Alejandro Lopez <alejandro.lopez@uabc.edu.mx>
2020-02-07 10:19:43 -0800

    Update cordova-hms-push.js

--- www/cordova-hms-push.js
+++ www/cordova-hms-push.js
@@ -12,6 +12,13 @@ HMSPush.prototype.tokenRegistered = function (token) {
         console.log('HuaweiPush:tokenRegistered ' + exception);
     }
 }
+HMSPush.prototype.onMessageReceived = function (pushData) {
+    try {
+        cordova.fireDocumentEvent('hmspush.onMessageReceived', pushData);
+    } catch(exception) {
+        console.log('HuaweiPush:onMessageReceived ' + exception);
+    }
+}
 HMSPush.prototype.init = function(success, error) {
     if (this.isAndroidDevice()) {
         exec(success, error, "CordovaHMSPush", "init", []);
