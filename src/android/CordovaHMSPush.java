14e2a62ab2ce7a9b6bc25e4ec9e5362fe4e8bdbb
Alejandro Lopez <alejandro.lopez@uabc.edu.mx>
2020-02-07 11:23:47 -0800

    Update CordovaHMSPush.java

--- src/android/CordovaHMSPush.java
+++ src/android/CordovaHMSPush.java
@@ -117,48 +117,52 @@ public class CordovaHMSPush extends CordovaPlugin {
             Log.e(TAG, "Received message entity is null!");
             return;
         }
-        Log.i(TAG, "getCollapseKey: " + message.getCollapseKey()
-                + "\n getData: " + message.getData()
-                + "\n getFrom: " + message.getFrom()
-                + "\n getTo: " + message.getTo()
-                + "\n getMessageId: " + message.getMessageId()
-                + "\n getOriginalUrgency: " + message.getOriginalUrgency()
-                + "\n getUrgency: " + message.getUrgency()
-                + "\n getSendTime: " + message.getSentTime()
-                + "\n getMessageType: " + message.getMessageType()
-                + "\n getTtl: " + message.getTtl());
-        RemoteMessage.Notification notification = message.getNotification();
-        JSONObject object = new JSONObject();
-        if (notification != null) {
-            Log.i(TAG, "\n getImageUrl: " + notification.getImageUrl()
-                    + "\n getTitle: " + notification.getTitle()
-                    + "\n getTitleLocalizationKey: " + notification.getTitleLocalizationKey()
-                    + "\n getTitleLocalizationArgs: " + Arrays.toString(notification.getTitleLocalizationArgs())
-                    + "\n getBody: " + notification.getBody()
-                    + "\n getBodyLocalizationKey: " + notification.getBodyLocalizationKey()
-                    + "\n getBodyLocalizationArgs: " + Arrays.toString(notification.getBodyLocalizationArgs())
-                    + "\n getIcon: " + notification.getIcon()
-                    + "\n getSound: " + notification.getSound()
-                    + "\n getTag: " + notification.getTag()
-                    + "\n getColor: " + notification.getColor()
-                    + "\n getClickAction: " + notification.getClickAction()
-                    + "\n getChannelId: " + notification.getChannelId()
-                    + "\n getLink: " + notification.getLink()
-                    + "\n getNotifyId: " + notification.getNotifyId());
-            object.put("title",notification.getTitle());
-            object.put("message",notification.getBody());
-        }
-        
-        
-        object.put("additionalData",message.getData());
-        String format = "window.cordova.plugins.hmspush.onMessageReceived(%s);";
-        final String js = String.format(format, object.toString());
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                instance.webView.loadUrl("javascript:" + js);
+        try{
+            Log.i(TAG, "getCollapseKey: " + message.getCollapseKey()
+                    + "\n getData: " + message.getData()
+                    + "\n getFrom: " + message.getFrom()
+                    + "\n getTo: " + message.getTo()
+                    + "\n getMessageId: " + message.getMessageId()
+                    + "\n getOriginalUrgency: " + message.getOriginalUrgency()
+                    + "\n getUrgency: " + message.getUrgency()
+                    + "\n getSendTime: " + message.getSentTime()
+                    + "\n getMessageType: " + message.getMessageType()
+                    + "\n getTtl: " + message.getTtl());
+            RemoteMessage.Notification notification = message.getNotification();
+            JSONObject object = new JSONObject();
+            if (notification != null) {
+                Log.i(TAG, "\n getImageUrl: " + notification.getImageUrl()
+                        + "\n getTitle: " + notification.getTitle()
+                        + "\n getTitleLocalizationKey: " + notification.getTitleLocalizationKey()
+                        + "\n getTitleLocalizationArgs: " + Arrays.toString(notification.getTitleLocalizationArgs())
+                        + "\n getBody: " + notification.getBody()
+                        + "\n getBodyLocalizationKey: " + notification.getBodyLocalizationKey()
+                        + "\n getBodyLocalizationArgs: " + Arrays.toString(notification.getBodyLocalizationArgs())
+                        + "\n getIcon: " + notification.getIcon()
+                        + "\n getSound: " + notification.getSound()
+                        + "\n getTag: " + notification.getTag()
+                        + "\n getColor: " + notification.getColor()
+                        + "\n getClickAction: " + notification.getClickAction()
+                        + "\n getChannelId: " + notification.getChannelId()
+                        + "\n getLink: " + notification.getLink()
+                        + "\n getNotifyId: " + notification.getNotifyId());
+                object.put("title",notification.getTitle());
+                object.put("message",notification.getBody());
             }
-        });
+
+
+            object.put("additionalData",message.getData());
+            String format = "window.cordova.plugins.hmspush.onMessageReceived(%s);";
+            final String js = String.format(format, object.toString());
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    instance.webView.loadUrl("javascript:" + js);
+                }
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         Boolean judgeWhetherIn10s = false;
         // If the messages are not processed in 10 seconds, the app needs to use WorkManager for processing.
         if (judgeWhetherIn10s) {
